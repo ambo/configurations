a0a7ff785b8a5719f0509a520df160e54b4a971e
Andreas Mängel <andreas.maengel@gmail.com>
2013-03-14 13:37:44 +0100

    changed git account information

--- bash_includes/git.sh
+++ bash_includes/git.sh
@@ -1,8 +1,8 @@
 ###########
 # GIT     #
 ###########
-export GIT_AUTHOR_NAME="Leon Weidauer"
-export GIT_AUTHOR_EMAIL="leon.weidauer@googlemail.com"
+export GIT_AUTHOR_NAME="Andreas Mängel"
+export GIT_AUTHOR_EMAIL="andreas.maengel@gmail.com"
 export GIT_COMMITER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITER_EMAIL="$GIT_AUTHOR_EMAIL"
 
