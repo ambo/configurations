0c126572720e4a9a6f260ee5413e707365297247
Andreas Mängel <andreas.maengel@berlinonline.de>
2013-11-14 09:54:11 +0100

    minor changes

--- bash_includes/env.sh
+++ bash_includes/env.sh
@@ -11,3 +11,6 @@ export EDITOR=vim
 # ignore case, long prompt, exit if it fits on one screen
 export LESS="-iMFXR"
 
+# drush path extension
+export PATH="$PATH:/Users/andreas/Documents/htdocs/drush:/usr/local/bin"
+export PATH="/Applications/MAMP/Library/bin:/Applications/MAMP/bin/php5.3/bin:$PATH"
--- bash_includes/git.sh
+++ bash_includes/git.sh
@@ -2,7 +2,7 @@
 # GIT     #
 ###########
 export GIT_AUTHOR_NAME="Andreas Mängel"
-export GIT_AUTHOR_EMAIL="andreas.maengel@gmail.com"
+export GIT_AUTHOR_EMAIL="andreas.maengel@berlinonline.de"
 export GIT_COMMITER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITER_EMAIL="$GIT_AUTHOR_EMAIL"
 
